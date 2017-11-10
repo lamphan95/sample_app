fcef61cf6076c0765e14e5ff083411d8f4c0da5a
Lam Phan <your.dinhlam29995@gmail.com>
2017-11-10 10:34:25 +0700

    Add hello

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  test "should get root" do
+    get "/"
+    assert_response :success
+  end
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+<<<<<<< e53e0eeb00591be3bd7c776f3f6b616ee589f079
+    assert_select "title", "#{@base_title}"
+=======
+    assert_select "title", "Home | #{@base_title}"
+>>>>>>> Finish static pages
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+end
